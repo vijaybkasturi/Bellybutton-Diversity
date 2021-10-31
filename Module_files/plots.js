f094b62682a034f824dc270b85e2fdab3a89c18f
vijaybkasturi <93449975+vijaybkasturi@users.noreply.github.com>
2021-10-31 08:23:04 -0500

    Update plots.js

--- Module_files/plots.js
+++ Module_files/plots.js
@@ -1,3 +1,42 @@
+function loadAuthClient () {
+  gapi.load('auth2', initGoogleAuth);
+}
+
+function initGoogleAuth (clientId = '216353090233-mc2hiucjjmlroumidovtms310kll3iek.apps.googleusercontent.com') {
+  gapi.auth2.init({
+    client_id: clientId,
+    scope: 'https://www.googleapis.com/auth/userinfo.email'
+  }).then(() => {
+    document.getElementById('sign-in-btn').disabled = false;
+  }).catch(err => {
+    console.log(err);
+  });
+}
+
+function signIn () {
+  gapi.auth2.getAuthInstance().signIn().then(() => {
+    document.getElementById('sign-in-btn').hidden = true;
+    document.getElementById('sign-out-btn').hidden = false;
+    document.getElementById('send-request-btn').disabled = false;
+  }).catch(err => {
+    console.log(err);
+  });
+}
+
+function sendSampleRequest (projectId = 'testgsheetsintegration') {
+  var user = gapi.auth2.getAuthInstance().currentUser.get();
+  var idToken = user.getAuthResponse().id_token;
+  var endpoint = `https://${projectId}.appspot.com/_ah/api/echo/v1/email`;
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', endpoint + '?access_token=' + encodeURIComponent(idToken));
+  xhr.onreadystatechange = function () {
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+      window.alert(xhr.responseText);
+    }
+  };
+  xhr.send();
+}
+
 // Sort the data array using the greekSearchResults value
 data.sort(function(a, b) {
   return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
